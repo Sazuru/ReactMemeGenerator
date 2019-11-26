b67e28ec467262c02b28decfc744c50c5060461e
Valentin Ageev <sazuru@gmail.com>
2019-11-26 23:00:22 +0300

    Updates

---
+++ precache-manifest.2f2a09d758fcf0f976520738b18968e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e99ac7551fa79996c20acdb61772b5",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "84bf3fe96258a4480fce",
+    "url": "/meme-generator/static/css/main.960a2a3b.chunk.css"
+  },
+  {
+    "revision": "d0b505fff14bb2c53b67",
+    "url": "/meme-generator/static/js/2.4893aea1.chunk.js"
+  },
+  {
+    "revision": "84bf3fe96258a4480fce",
+    "url": "/meme-generator/static/js/main.6f7521a0.chunk.js"
+  },
+  {
+    "revision": "96b91e0b7471731335ed",
+    "url": "/meme-generator/static/js/runtime-main.d5442b6b.js"
+  }
+]);
