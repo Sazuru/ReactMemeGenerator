b848f765a6b8861452c47a57a4b43501148cbbef
Valentin Ageev <sazuru@gmail.com>
2019-11-26 23:14:58 +0300

    Test commit

---
+++ build/precache-manifest.301718d8f45ea8a698e0955b669811f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fd397268e7f05cbfc65ec8cb0683e4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84bf3fe96258a4480fce",
+    "url": "/static/css/main.960a2a3b.chunk.css"
+  },
+  {
+    "revision": "d0b505fff14bb2c53b67",
+    "url": "/static/js/2.4893aea1.chunk.js"
+  },
+  {
+    "revision": "84bf3fe96258a4480fce",
+    "url": "/static/js/main.6f7521a0.chunk.js"
+  },
+  {
+    "revision": "9a63796aa62d5f4027a3",
+    "url": "/static/js/runtime-main.f365db67.js"
+  }
+]);
